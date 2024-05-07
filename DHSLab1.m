48fe9e05a1e30cafc808feade0dd72e8c5b4fb31
Chesful <jesus.nieto.rojo@gmail.com>
2024-05-07 18:07:01 +0200

    Create DHSLab1.m

---
+++ DHSLab1.m
@@ -0,0 +1,22 @@
+clc; clear;
+m=csvread("measurement.txt",1)
+figure(1);
+plot(m(:,1),m(:,2))
+hold on;title("Temperature Fixed");xlabel('Time [m]');ylabel('Temperature [ºC]'); hold off;
+
+n=NaN;
+for i=1:length(m)
+    if m(i,2)>1
+        if ~isnan(n)
+            n=[n;m(i,1),m(i,2)];
+        else
+            n=[m(i,1),m(i,2)];
+        end
+    end
+end
+figure(2);
+plot(n(:,1),n(:,2));
+hold on;title("Temperature Fixed");xlabel('Time [s]');ylabel('Temperature [ºC]'); hold off;
+figure(3);
+plot(m(:,1),m(:,3))
+hold on;title("Luminosity");xlabel('Time [s]');ylabel('Luminosity [Lux]'); hold off;
